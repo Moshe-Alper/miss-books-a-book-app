9ea4eb3c300943bbb377737f8f4593cab44a1f25
Moshik Alper <moshikalper@gmail.com>
2024-09-26 16:19:06 +0300

    Style header

--- pages/BookEdit.jsx
+++ pages/BookEdit.jsx
@@ -31,7 +31,7 @@ export function BookEdit() {
         switch (target.type) {
             case 'number':
             case 'range':
-                value = +value
+                value = target.value === '' ? '' : +value
                 break;
 
             case 'checkbox':
