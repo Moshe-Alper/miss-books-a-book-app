ec6dad9dd9e895eef06157e1b6fd918f6499cde0
Moshik Alper <moshikalper@gmail.com>
2024-09-30 16:10:35 +0300

    Style review and chart

--- pages/Dashboard.jsx
+++ pages/Dashboard.jsx
@@ -1,9 +1,9 @@
 const {useEffect, useState} = React
+
+import { Chart } from '../cmps/Chart.jsx'
 import { bookService } from '../services/book.service.js'
 
-
 export function Dashboard() {
-
 
     const [books, setBooks] = useState([])
     const [categoryStats, setCategoryStats] = useState([])
@@ -21,6 +21,8 @@ export function Dashboard() {
             <h1>Dashboard</h1>
             <h2>Statistics for {books.length} Books</h2>
             <h4>By Category</h4>
+            <Chart data={categoryStats} />
+
         </section>
     )
 }
