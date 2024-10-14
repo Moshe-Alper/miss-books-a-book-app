9904b95f782503fa81136cac8f20d509e7c0d1b1
Moshik Alper <moshikalper@gmail.com>
2024-10-14 15:31:11 +0300

    Add link to the main app (appsus)

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,23 +1,29 @@
-
 const { NavLink } = ReactRouterDOM
 
 export function AppHeader() {
-
-
     const links = [
-        { title: 'Home', href: '/homePage' },
-        { title: 'About', href: '/aboutUs' },
-        { title: 'Dashboard', href: '/dashboard' },
-        { title: 'Book', href: '/bookIndex' }
+        { title: 'Back to Appsus', href: 'https://moshe-alper.github.io/Proj-Appsus-24/', external: true },
+        // { title: 'Home', href: '/homePage', external: false },
+        // { title: 'About', href: '/aboutUs', external: false },
+        { title: 'Dashboard', href: '/dashboard', external: false },
+        { title: 'Book', href: '/bookIndex', external: false }
     ]
-    
+
     return (
         <header className="app-header full main-layout">
             <section>
                 <h1 className="logo">Miss Books</h1>
                 <nav className="app-nav">
                     {links.map((link, idx) => {
-                        return <NavLink to={link.href} key={idx} title={link.title}>{link.title}</NavLink>
+                        return link.external ? (
+                            <a href={link.href} key={idx} title={link.title} target="_blank" rel="noopener noreferrer">
+                                {link.title}
+                            </a>
+                        ) : (
+                            <NavLink to={link.href} key={idx} title={link.title}>
+                                {link.title}
+                            </NavLink>
+                        )
                     })}
                 </nav>
             </section>
